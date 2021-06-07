99e491a5f74c16247a3cc49cb225d3146751211e
Nivedita-PA <npal4661@gmail.com>
2021-06-07 23:28:11 +0530

    Test case introduced for returning max 2nd value

--- src/test/java/Generics/MaxTest.java
+++ src/test/java/Generics/MaxTest.java
@@ -12,4 +12,12 @@ public class MaxTest {
         boolean status = result.equals(5);
         Assert.assertTrue(status);
     }
+
+    @Test
+    public void givenMaxNum_Should_Return_SecondNum(){
+        Max max = new Max();
+        Integer result = max.Intmax(5,6,3);
+        boolean status = result.equals(6);
+        Assert.assertTrue(status);
+    }
 }
