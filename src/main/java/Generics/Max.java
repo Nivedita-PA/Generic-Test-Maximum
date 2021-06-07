bc55fc578a68b3dc3a642de8fcf5e0d1ab180876
Nivedita-PA <npal4661@gmail.com>
2021-06-07 23:38:10 +0530

    Test case along with the method comparing float values introduced as with Integers

--- src/main/java/Generics/Max.java
+++ src/main/java/Generics/Max.java
@@ -9,12 +9,28 @@ public class Max {
         if (c.compareTo(max) > 0) {
             max = c;
         }
-        printInt(a,b,c,max);
+        printInt(a, b, c, max);
         return max;
     }
 
-    public static void printInt(Integer a, Integer b, Integer c, Integer max){
-        System.out.println("The largest integer value among " +a +" ,"+b+" ," +c+" " +"is " +max);
+    public static void printInt(Integer a, Integer b, Integer c, Integer max) {
+        System.out.println("The largest integer value among " + a + " ," + b + " ," + c + " " + "is " + max);
+    }
+
+    public static Double Doublemax(Double e, Double i, Double o) {
+        Double max = e;
+        if (i.compareTo(max) > 0) {
+            max = i;
+        }
+        if (o.compareTo(max) > 0) {
+            max = o;
+        }
+        printDbl(e, i, o, max);
+        return max;
+    }
+
+    public static void printDbl(Double e, Double i, Double o, Double max) {
+        System.out.println("The largest float value among " + e + ", " + i + " ," + o + " " + "is " + max);
     }
 
 }
--- src/test/java/Generics/MaxTest.java
+++ src/test/java/Generics/MaxTest.java
@@ -28,4 +28,28 @@ public class MaxTest {
         boolean status = result.equals(7);
         Assert.assertTrue(status);
     }
+
+    @Test
+    public void givenMaxFloat_Should_Return_FirstValue(){
+        Max max3 = new Max();
+        Double result = max3.Doublemax(8.5,7.5,5.9);
+        boolean status = result.equals(8.5);
+        Assert.assertTrue(status);
+    }
+    @Test
+    public void givenMaxFloat_Should_Return_SecondValue(){
+        Max max3 = new Max();
+        Double result = max3.Doublemax(5.5,6.5,4.5);
+        boolean status = result.equals(6.5);
+        Assert.assertTrue(status);
+    }
+    @Test
+    public void givenMaxFloat_Should_Return_ThirdValue(){
+        Max max3 = new Max();
+        Double result = max3.Doublemax(4.9,8.9,10.1);
+        boolean status = result.equals(10.1);
+        Assert.assertTrue(status);
+    }
+
+
 }
