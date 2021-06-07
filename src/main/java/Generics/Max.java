2d995a4c616d4d846e41ec503a264a1f13aa19ec
Nivedita-PA <npal4661@gmail.com>
2021-06-07 23:12:56 +0530

    Introduced three integers returning max value

--- src/main/java/Generics/Max.java
+++ src/main/java/Generics/Max.java
@@ -1,5 +1,20 @@
 package Generics;
 
 public class Max {
+    public static Integer Intmax(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        printInt(a,b,c,max);
+        return max;
+    }
+
+    public static void printInt(Integer a, Integer b, Integer c, Integer max){
+        System.out.println("The largest integer value among " +a +" ,"+b+" ," +c+" " +"is " +max);
+    }
 
 }
