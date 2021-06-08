6567139722f60587cbd9902dbb68cfecb774e028
Nivedita-PA <npal4661@gmail.com>
2021-06-08 23:45:32 +0530

    Calling Print method for the maximum value using generic objects in main method

--- src/main/java/Generics/Max2.java
+++ src/main/java/Generics/Max2.java
@@ -17,16 +17,31 @@ public class Max2<E extends Comparable> {
         if (c.compareTo(max) > 0) {
             max = c;
         }
-        //printMax(a, b, c, max);
+        printMax(a, b, c, max);
         return max;
     }
 
-//    public static <E> void printMax(E a, E b, E c, E max) {
-//        System.out.println("The max value of the generic variables " + a + "," + b + "," + c + " " + "is " + max);
-//    }
+    public static <E> void printMax(E a, E b, E c, E max) {
+        System.out.println("The max value of the generic variables " + a + "," + b + "," + c + " " + "is " + max);
+    }
 
     public E toPrint() {
         return genMax(a, b, c);
+    }
+
+    public static void main(String[] args) {
+        Integer a =4; Integer b =6; Integer c = 8;
+        genMax(a,b,c);
+        String x = "orange" ,y = "apple", z = "banana";
+        genMax(x,y,z);
+        Double e = 1.1, i = 2.2, o = 3.3;
+        genMax(e,i,o);
+        Max2<Integer> max = new Max2(2,3,4);
+        max.toPrint();
+        Max2<Double> max2 = new Max2(3.2,4.5,8.5);
+        max2.toPrint();
+        Max2<String> max3 = new Max2("orange","apple","banana");
+        max3.toPrint();
     }
 
 }
